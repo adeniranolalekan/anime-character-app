cda293cbef6907339de0033d8934c66ee72d9186
olamilekan <adeniranolalekan35@gmail.com>
2020-05-18 19:16:28 +0100

    add custom scroll bar

--- src/Group.js
+++ src/Group.js
@@ -1,7 +1,7 @@
 import React from "react";
 const Group=({children})=>{
 
-    return <div className="Group">
+    return <div className="Group styled-scroll-bar" >
         {children}
     </div>
 }
